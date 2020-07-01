c69e2ffb8ab1fa7ca5ea5e2e8ec13542cb7b5b47
Charlie Billadeau <charlie.billadeau@gmail.com>
2020-07-01 11:13:48 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a4d28d070921a0b8e5fc475ca68541a5.js"
+  "/portfolio/precache-manifest.7a7c84b4834b8ef84fa133b10e885b74.js"
 );
 
 self.addEventListener('message', (event) => {
